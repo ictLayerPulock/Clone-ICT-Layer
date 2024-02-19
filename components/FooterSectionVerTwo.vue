c0a8049ae8d2393532941610cae3a74d6f552bd5
ictLayerPulock <157199030+ictLayerPulock@users.noreply.github.com>
2024-02-19 12:18:37 +0600

    new way

--- components/FooterSectionVerTwo.vue
+++ components/FooterSectionVerTwo.vue
@@ -3,8 +3,8 @@
     <div class="bg-gray-50">01</div>
     <div class="bg-orange-600">
       <div class="grid grid-cols-3">
-        <div class="bg-gray-300 place-self-end">
-          <img class="" height="100%" src="../assets/img/logo.png" alt="" />
+        <div class="bg-gray-300">
+          <img class="" src="../assets/img/logo.png" alt="" />
         </div>
         <div class="bg-orange-600 col-span-2">02</div>
       </div>
@@ -19,13 +19,13 @@
         </div>
         <div class="row-span-3 bg-[#565a60]">
           <div class="px-4">
-            <p class="text-gray-200">Advance Network Development (AND Ltd.)</p>
+            <p class="text-gray-200 pt-2">Advance Network Development (AND Ltd.)</p>
             <p class="text-gray-300 text-sm">Freelancing Department</p>
-            <p class="text-gray-200">Social Lancer</p>
+            <p class="text-gray-200 pt-3">Social Lancer</p>
             <p class="text-gray-300 text-sm">
               Social Media Marketing Department Bangladesh
             </p>
-            <p class="text-gray-200">Cloud Oracle Hosting</p>
+            <p class="text-gray-200 pt-3">Cloud Oracle Hosting</p>
             <p class="text-gray-300 text-sm">Web Hosting Department</p>
           </div>
         </div>
@@ -39,7 +39,7 @@
         <div class="row-span-3 bg-[#ab5941]">
           <div class="p-5">
             <p class="text-gray-200">149, Senpara Parbata, Mirpur, Dhaka, BD</p>
-            <p class="text-gray-200">+880 1844-096540, +1(234) 770-0440</p>
+            <p class="text-gray-200 py-5">+880 1844-096540, +1(234) 770-0440</p>
             <p class="text-gray-200">info@ictlayer.com, ictlayer@gmail.com</p>
           </div>
         </div>
@@ -47,7 +47,7 @@
     </div>
     <div class="bg-[#27282a]">05</div>
     <div class="bg-orange-600">
-      <ul class="flex items-center gap-3 p-8">
+      <ul class="flex items-center pl-10 gap-2">
         <li class=" ">
           <img
             class="p-1 mx-auto rounded-full ring-2 ring-neutral-50 hover:bg-gray-600 sm:mx-0 sm:flex-shrink-0 transform hover:scale-105 duration-500"
@@ -81,7 +81,17 @@
   </div>
 
   <div class="grid grid-cols-5 h-28">
-    <div class=" col-span-3 bg-[#27282a]">01</div>
+    <div class="  bg-[#27282a]">01</div>
+    <div class="  bg-[#27282a]">
+      <ul class="flex items-center pt-10 gap-3 text-gray-400 text-sm">
+          <li class="hover:text-orange-600"><a href="">Blog & News</a></li>
+          <li class="hover:text-orange-600">
+            <a href="">Contuct & Support</a>
+          </li>
+          <li class="hover:text-orange-600"><a href="">Domain & Hosting</a></li>
+        </ul>
+    </div>
+    <div class="  bg-[#27282a]">01</div>
     <div class="col-span-2 bg-[#733927]">02</div>
   </div>
 </template>
