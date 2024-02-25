a76a600f6d6da2908240826bffe42073c71bc70c
ictLayerPulock <157199030+ictLayerPulock@users.noreply.github.com>
2024-02-25 12:01:25 +0600

    marketing section

--- components/AddMarketing.vue
+++ components/AddMarketing.vue
@@ -3,7 +3,7 @@
     <div
       class="bg-cover"
       style="
-        background-image: url(https://images.pexels.com/photos/13918756/pexels-photo-13918756.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
+        background-image: url(../assets/img/feature7-bg.jpg);
       "
     >
       <div class="">
@@ -12,7 +12,7 @@
             <div class="bg-orange-600 h-full ml-10 mx-20">
               <div class="mx-10">
                 <p class="text-white text-2xl py-5">DO YOUR WANT</p>
-                <p class="text-white text-2xl">FACEOOK ADS MARKETING/</p>
+                <p class="text-white text-2xl">FACEBOOK ADS MARKETING/</p>
                 <p class="text-white text-2xl py-5">GOOGLE ADS MARKETING</p>
                 <p class="text-white text-2xl">IN BANGLADESH?</p>
                 <br />
